93bcb507b182459838e457f6a6d58414739ebab2
filip <fbielejec@gmail.com>
2020-07-02 09:29:39 +0200

    version++

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject district0x/mount "0.1.17"
+(defproject district0x/mount "0.1.18-SNAPSHOT"
   :description "managing Clojure and ClojureScript app state since (reset)"
   :url "https://github.com/tolitius/mount"
   :license {:name "Eclipse Public License"
